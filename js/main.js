a76410b76c4a2467b4f385ca7833d70afcdb78b2
NIROSHA.N <59044022+parthinirosha25@users.noreply.github.com>
2021-07-31 11:07:06 +0530

    19

--- js/main.js
+++ js/main.js
@@ -134,13 +134,13 @@ jQuery(document).ready(function( $ ) {
     }
   });
 
-  /* // Buy tickets select the ticket type on click
+  // Buy tickets select the ticket type on click
   $('#buy-ticket-modal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget);
     var ticketType = button.data('ticket-type');
     var modal = $(this);
     modal.find('#ticket-type').val(ticketType);
-  }) */
+  })
 
 // custom code
 
